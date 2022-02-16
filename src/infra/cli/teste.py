f53f13335422767e47d1ff57775828c70b023423
Diqxy1 <kusanagitakeru@outlook.com>
2022-02-16 15:33:55 -0300

    update cli

--- src/infra/cli/teste.py
+++ src/infra/cli/teste.py
@@ -5,6 +5,6 @@ app = typer.Typer()
 @app.command()
 def validations():
     i = 0
-    while i >= 100000:
-        typer.secho(i)
+    while i <= 100000:
+        typer.secho(f'{i}')
         i += 1
